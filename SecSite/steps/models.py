55e3ee5d9845070cbc4a4d2646c95c6058a550ac
StupidProger-Lamer <grisga2001@gmail.com>
2020-01-09 06:29:58 +0700

    some changings

--- SecSite/steps/models.py
+++ SecSite/steps/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AdvUser(models.Model):
+	is_activated = models.BooleanField(default = True)
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+class Spare(models.Model):
+	name = models.CharField(max_length = 30)
+
+class Machine(models.Model):
+	name = models.CharField(max_length = 30)
+	spares = models.ManyToManyField(Spare)
+
+class Steps(models.Model):
+	title = models.CharField(verbose_name = 'Название проекта', max_length = 100)
+	content = models.TextField(verbose_name = 'Описание работы', null = True, blank = True)
+	published = models.DateTimeField(verbose_name = 'Дата публикации', auto_now_add = True, db_index = True)
+	theme = models.ForeignKey('Themes', on_delete = models.PROTECT, null = True, verbose_name = 'Тема')
+	class Meta:
+		verbose_name = 'Проект'
+		verbose_name_plural = 'Проекты'
+		ordering = ['-published']
+
+class Themes(models.Model):
+	name = models.CharField(verbose_name = 'Тема проекта', max_length = 50, db_index = True)
+	def __str__(self):
+		return self.name
+	class Meta:
+		verbose_name = 'Тема'
+		verbose_name_plural = 'Темы'
+		ordering = ['name']
+
+
