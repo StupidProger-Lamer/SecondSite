761af3ccecfab383cdea90269f7f8755438e5d05
StupidProger-Lamer <grisga2001@gmail.com>
2020-01-13 05:16:07 +0700

    del couple useless models

--- SecSite/steps/models.py
+++ SecSite/steps/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class AdvUser(models.Model):
-	is_activated = models.BooleanField(default = True)
-	user = models.OneToOneField(User, on_delete = models.CASCADE)
-
-class Spare(models.Model):
-	name = models.CharField(max_length = 30)
-
-class Machine(models.Model):
-	name = models.CharField(max_length = 30)
-	spares = models.ManyToManyField(Spare)
-class ClassName(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
 		
 class Steps(models.Model):
 	title = models.CharField(verbose_name = 'Название проекта', max_length = 100)
